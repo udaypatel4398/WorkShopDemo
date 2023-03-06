84bbdf3d90cc762c671183ed8c3df68067be0932
udaypatel4398 <udayajani4398@gmail.com>
2023-03-06 16:32:56 +0530

    Project Upload

---
+++ WorkShopDemo/WorkShopDemo/Data/WorkShopDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorkShopDemo.Models;
+
+namespace WorkShopDemo.Data
+{
+    public class WorkShopDemoContext : DbContext
+    {
+        public WorkShopDemoContext (DbContextOptions<WorkShopDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WorkShopDemo.Models.Car> Car { get; set; }
+    }
+}
---
+++ WorkShopDemo/WorkShopDemo/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkShopDemo.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Registration { get; set; }
+        public DateTime LastService { get; set; }
+        public bool IsInWorkshop { get; set; }
+    }
+}
